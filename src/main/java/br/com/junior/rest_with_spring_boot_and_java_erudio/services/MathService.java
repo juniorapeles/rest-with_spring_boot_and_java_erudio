0ebd2987a148b836c00290b31c18ae5a13701239
juniorapeles <elijunior.apeles2005@gmail.com>
2025-09-06 23:45:31 -0300

    REFACTOR: removendo validação do service pois o controller ficou com a dependência

--- src/main/java/br/com/junior/rest_with_spring_boot_and_java_erudio/services/MathService.java
+++ src/main/java/br/com/junior/rest_with_spring_boot_and_java_erudio/services/MathService.java
@@ -1,6 +1,5 @@
 package br.com.junior.rest_with_spring_boot_and_java_erudio.services;
 
-import br.com.junior.rest_with_spring_boot_and_java_erudio.commons.IMathValidator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,11 +7,6 @@ public class MathService implements IMathService {
 
     private static final int AVERAGE_NUMBER = 2;
 
-    private final IMathValidator validator;
-
-    public MathService(IMathValidator validator) {
-        this.validator = validator;
-    }
 
     @Override
     public Double sum(Double first, Double second) {
@@ -31,13 +25,11 @@ public class MathService implements IMathService {
 
     @Override
     public Double div(Double first, Double second) {
-        validator.validateNonZero(second);
         return first / second;
     }
 
     @Override
     public Double sqrt(Double number) {
-        validator.validateNonNegative(number);
         return Math.sqrt(number);
     }
 
