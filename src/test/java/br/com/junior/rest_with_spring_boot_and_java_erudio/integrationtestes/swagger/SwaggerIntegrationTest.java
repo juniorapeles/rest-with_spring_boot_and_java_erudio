4f985382699ba29d2531b6e22291cb107e0d1ea4
juniorapeles <elijunior.apeles2005@gmail.com>
2025-10-21 19:14:47 -0300

    fix: ajuste test-containers

--- src/test/java/br/com/junior/rest_with_spring_boot_and_java_erudio/integrationtestes/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/junior/rest_with_spring_boot_and_java_erudio/integrationtestes/swagger/SwaggerIntegrationTest.java
@@ -2,11 +2,13 @@ package br.com.junior.rest_with_spring_boot_and_java_erudio.integrationtestes.sw
 
 import br.com.junior.rest_with_spring_boot_and_java_erudio.config.TestConfigs;
 import br.com.junior.rest_with_spring_boot_and_java_erudio.integrationtestes.testcontainers.AbstractIntegrationTest;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.port;
 import static junit.framework.TestCase.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -14,16 +16,17 @@ class SwaggerIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     void shouldDisplaySwaggerUIPage() {
-        var content = given()
-                .baseUri("/swagger-ui/index.html")
+        String content = RestAssured.given()
+                .baseUri("http://localhost")
                 .port(TestConfigs.SERVER_PORT)
                 .when()
-                .get()
+                .get("/swagger-ui/index.html")
                 .then()
                 .statusCode(200)
                 .extract()
                 .body()
                 .asString();
+
 
         Assertions.assertTrue(content.contains("Swagger UI"));
     }
